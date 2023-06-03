3d7f35b0b01dbc7424ab2ad20c7d848d6f81bda3
tolunayg <tolunaygedikoglu@gmail.com>
2023-06-03 21:07:49 +0300

    Initial commit

---
+++ src/test/java/tr/edu/metu/sm703/HandsOnOneTest.java
@@ -0,0 +1,21 @@
+package tr.edu.metu.sm703;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class HandsOnOneTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
