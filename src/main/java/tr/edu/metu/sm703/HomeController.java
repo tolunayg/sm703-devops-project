15ba0203771c2a5096a76428149d5a7152aa7d74
tolunayg <tolunaygedikoglu@gmail.com>
2023-06-11 13:41:43 +0300

    Trying to implement security vulnerability to be detected with SAST scan

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -11,16 +11,23 @@ public class HomeController {
 
     @Get("/add/{num1}/{num2}")
     public Map<String, Object> addNumbers(@PathVariable int num1, @PathVariable int num2) {
-        // Check if num1 or num2 are invalid
-        if (num1 < 0 || num2 < 0) {
-            throw new IllegalArgumentException("Invalid input: num1 and num2 must be non-negative integers.");
-        }
+        String username = "admin";
+        String password = "password123";
 
-        int sum = num1 + num2;
-        Map<String, Object> response = new HashMap<>();
-        response.put("num1", num1);
-        response.put("num2", num2);
-        response.put("sum", sum);
-        return response;
+        // Check if the provided username and password match the hardcoded credentials
+        if (username.equals("admin") && password.equals("password123")) {
+            // Perform the addition operation
+            int sum = num1 + num2;
+            Map<String, Object> response = new HashMap<>();
+            response.put("num1", num1);
+            response.put("num2", num2);
+            response.put("sum", sum);
+            return response;
+        } else {
+            // Return an error message if the credentials are incorrect
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("error", "Invalid credentials");
+            return errorResponse;
+        }
     }
 }
