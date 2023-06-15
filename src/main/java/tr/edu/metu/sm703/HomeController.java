a5db82a093c23dbe254e5d2db05d86eaa344774e
tolunayg <tolunaygedikoglu@gmail.com>
2023-06-16 02:34:51 +0300

    Changed system.out.println with logger

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -5,15 +5,20 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import java.util.HashMap;
 import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Controller
 public class HomeController {
+
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 
     @Get("/add/{num1}/{num2}")
     public Map<String, Object> addNumbers(@PathVariable int num1, @PathVariable int num2) {
         // Perform the addition operation
         int sum = num1 + num2;
-        System.out.println("New addition request! Adding " + num1 + " " + num2 + ". Result is: " + sum);
+        logger.info("New addition request! Adding {} {}. Result is: {}", num1, num2, sum);
         Map<String, Object> response = new HashMap<>();
         response.put("num1", num1);
         response.put("num2", num2);
