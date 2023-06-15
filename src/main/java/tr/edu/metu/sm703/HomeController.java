f61bd8f66621332662d11484f069e71c3b3d8c23
tolunayg <tolunaygedikoglu@gmail.com>
2023-06-16 00:48:57 +0300

    Introduce code issues for SAST step, to be caught by SonarQube

--- src/main/java/tr/edu/metu/sm703/HomeController.java
+++ src/main/java/tr/edu/metu/sm703/HomeController.java
@@ -9,6 +9,8 @@ import java.util.Map;
 @Controller
 public class HomeController {
 
+    private String password = "myPassword123"; // Vulnerability: Storing password in plain text
+
     @Get("/add/{num1}/{num2}")
     public Map<String, Object> addNumbers(@PathVariable int num1, @PathVariable int num2) {
         // Perform the addition operation
@@ -18,7 +20,39 @@ public class HomeController {
         response.put("num2", num2);
         response.put("sum", sum);
 
+        // TODO: Todo example to be detected by codeQL
         return response;
     }
 
+    @Get("/users/{id}")
+    public Map<String, Object> getUserById(@PathVariable String id) {
+        // Insecure user authentication
+        boolean authenticated = authenticateUser(id, password); // Vulnerability: Insecure authentication
+
+        if (authenticated) {
+            // Fetch and return user data
+            Map<String, Object> userData = fetchUserData(id);
+            return userData;
+        } else {
+            // Return an error message if authentication fails
+            Map<String, Object> errorResponse = new HashMap<>();
+            errorResponse.put("error", "Authentication failed");
+            return errorResponse;
+        }
+    }
+
+    private boolean authenticateUser(String id, String password) {
+        // Insecure authentication logic
+        // In this example, authentication is based on a fixed password without encryption or validation
+        return password.equals("myPassword123"); // Vulnerability: Insecure password comparison
+    }
+
+    private Map<String, Object> fetchUserData(String id) {
+        // Simulated method to fetch user data from a database
+        // This method is not implemented for simplicity
+        // ...
+        // Potential vulnerability: The 'id' parameter is not validated or sanitized before using it in the query
+        // ...
+        return new HashMap<>();
+    }
 }
